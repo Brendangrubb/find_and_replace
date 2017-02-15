527485bf4d02024de384730c7b68600b641e0f2d
Brendan Grubb and Charles Peden <charles@>
2017-02-15 11:34:31 -0800

    add code/test to implement replace functionality for whole word

--- src/Find.php
+++ src/Find.php
@@ -8,10 +8,14 @@
             {
                 if ($word == $search_word)
                 {
-                    return $word;
+                    $var_index = array_search($word, $split_input_string);
+                    $split_input_string[$var_index] = $replace_word;
                 }
 
             }
+            // echo "is it here? " . $split_input_string;
+            $return_string = implode(" ", $split_input_string);
+            return $return_string;
         }
     }
 
--- tests/FindTest.php
+++ tests/FindTest.php
@@ -8,12 +8,24 @@
             $test_Find = new Find;
             $input1 = "I like pie";
             $input2 = "pie";
-            $input3 = "";
+            $input3 = "pie";
 
 
             $result = $test_Find->findAndReplace($input1, $input2, $input3);
 
-            $this->assertEquals("pie", $result);
+            $this->assertEquals("I like pie", $result);
+        }
+
+        function test_findAndReplace_findAndReplace()
+        {
+            $test_Find = new Find;
+            $input1 = "I like pie";
+            $input2 = "pie";
+            $input3 = "cake";
+
+            $result = $test_Find->findAndReplace($input1, $input2, $input3);
+
+            $this->assertEquals("I like cake", $result);
         }
     }
 
